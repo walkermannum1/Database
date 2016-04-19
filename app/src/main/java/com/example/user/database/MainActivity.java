245990e7ef13aa875b70ae9c7589ef6c4f7e010b
walkermannum1 <walkermannum1>
2016-04-19 10:47:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/database/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.user.database;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/*2016/4/17/17:01*/
+
+public class MainActivity extends Activity {
+    private MyDatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+    }
+}
