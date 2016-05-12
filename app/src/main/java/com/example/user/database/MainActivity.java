e2d9254ccf72a02a8c84ff99e9fbe4efc66670e3
walkermannum1 <walkermannum1>
2016-05-12 16:57:41 +0800

    plus

--- app/src/main/java/com/example/user/database/MainActivity.java
+++ app/src/main/java/com/example/user/database/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.user.database;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,26 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                //frist data
+                values.put("name", "Compelete Code");
+                values.put("author", "code");
+                values.put("pages", 400);
+                values.put("price", 26);
+                db.insert("Book", null, values);
+                //second data
+                values.put("name", "Thinking in java");
+                values.put("author", "manald");
+                values.put("pages", 215);
+                values.put("price", 23);
+                db.insert("Book", null, values);
             }
         });
     }
